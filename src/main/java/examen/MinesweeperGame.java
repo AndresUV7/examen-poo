5e594783e3860ef99daff73297c18dc6d531172a
AndresUV7 <andres.ullauri18@gmail.com>
2024-11-26 09:34:30 -0500

    chore: Inicializar configuración del proyecto Java

---
+++ src/main/java/examen/MinesweeperGame.java
@@ -0,0 +1,10 @@
+package examen;
+
+//Clase principal para iniciar el juego de Buscaminas
+public class MinesweeperGame {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al juego de Buscaminas");
+	}
+
+}
